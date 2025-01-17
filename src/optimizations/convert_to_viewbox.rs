1f60f39d5991b5e87514ec077969d8a84029a9b8
Paweł Kochański <pawilioxion@gmail.com>
2025-01-17 14:13:59 +0100

    Added other length units to convert_to_viewbox

--- src/optimizations/convert_to_viewbox.rs
+++ src/optimizations/convert_to_viewbox.rs
@@ -1,21 +1,43 @@
 use super::common::iter::EasyIter;
 use crate::node::{Node, RegularNodeType};
 use anyhow::Result;
+use regex::Regex;
 use xml::{attribute::OwnedAttribute, name::OwnedName};
 
 const WIDTH_NAME: &str = "width";
 const HEIGHT_NAME: &str = "height";
 const VIEWBOX_NAME: &str = "viewBox";
 
+fn unit_to_multiplier(unit: &str) -> Option<f64> {
+    // Convert only units which would realistically shorten the value
+    match unit {
+        "px" | "" => Some(1.),
+        "pt" => Some(1.25),
+        "pc" => Some(15.),
+        _ => None,
+    }
+}
+
 fn get_dimensions(attributes: &[OwnedAttribute]) -> (Option<f64>, Option<f64>) {
-    let find_float_attr = |name: &str| {
+    let match_val_and_unit: Regex = Regex::new(r"(.*?)([^\d\.]*)$").unwrap();
+
+    let find_and_convert_to_px = |name: &str| -> Option<f64> {
         attributes
             .iter()
             .find(|attribute| attribute.name.local_name == name)
-            .and_then(|attr| attr.value.parse::<f64>().ok())
+            .and_then(|attr| match_val_and_unit.captures(&attr.value))
+            .map(|capture| capture.extract())
+            .and_then(|(_, [val, unit])| {
+                let base_val = val.parse::<f64>().ok()?;
+                let mult = unit_to_multiplier(unit)?;
+                Some(base_val * mult)
+            })
     };
 
-    (find_float_attr(WIDTH_NAME), find_float_attr(HEIGHT_NAME))
+    (
+        find_and_convert_to_px(WIDTH_NAME),
+        find_and_convert_to_px(HEIGHT_NAME),
+    )
 }
 
 fn convert_to_viewbox_from_node(node: Node) -> Node {
@@ -89,6 +111,21 @@ mod tests {
         "#,
         r#"
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">
+        <ellipse rx="50" cx="100" ry="50" cy="50"/>
+        </svg>
+        "#
+    );
+
+    test_optimize!(
+        test_convert_to_viewbox_other_units,
+        convert_to_viewbox,
+        r#"
+        <svg xmlns="http://www.w3.org/2000/svg" width="200px" height="10pc">
+        <ellipse rx="50" cx="100" ry="50" cy="50"/>
+        </svg>
+        "#,
+        r#"
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 150">
         <ellipse rx="50" cx="100" ry="50" cy="50"/>
         </svg>
         "#
