f778f2e632598b4cdfc01fb3fcd95b7974b88109
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2025-01-20 21:34:52 +0100

    Implemented rest of merge_transforms

--- src/optimizations/merge_transforms.rs
+++ src/optimizations/merge_transforms.rs
@@ -2,14 +2,9 @@ use super::common::{constants::TRANSFORM_NAME, id_usage::find_attribute_mut, ite
 use crate::node::Node;
 use itertools::Itertools;
 use lazy_regex::regex;
-use nalgebra::Matrix3;
+use nalgebra::{Matrix2, Matrix3, Vector2};
 
-fn create_matrix(arguments: &str) -> Option<Matrix3<f64>> {
-    let arguments: Vec<f64> = arguments
-        .split_whitespace()
-        .filter_map(|arg| arg.parse::<f64>().ok())
-        .collect();
-
+fn matrix(arguments: Vec<f64>) -> Option<Matrix3<f64>> {
     if let [a, b, c, d, e, f] = arguments[..] {
         Some(Matrix3::from_vec(vec![a, b, 0., c, d, 0., e, f, 1.]))
     } else {
@@ -17,20 +12,69 @@ fn create_matrix(arguments: &str) -> Option<Matrix3<f64>> {
     }
 }
 
+fn translate(arguments: Vec<f64>) -> Option<Matrix3<f64>> {
+    match arguments[..] {
+        [x, y] => Some(Matrix3::new_translation(&Vector2::new(x, y))),
+        [x] => Some(Matrix3::new_translation(&Vector2::new(x, x))),
+        _ => None,
+    }
+}
+
+fn scale(arguments: Vec<f64>) -> Option<Matrix3<f64>> {
+    match arguments[..] {
+        [x, y] => Some(Matrix3::new_nonuniform_scaling(&Vector2::new(x, y))),
+        [x] => Some(Matrix3::new_scaling(x)),
+        _ => None,
+    }
+}
+
+fn rotate(arguments: Vec<f64>) -> Option<Matrix3<f64>> {
+    match arguments[..] {
+        [a, x, y] => Some(
+            Matrix3::new_translation(&Vector2::new(x, y))
+                * Matrix3::new_rotation(a.to_radians())
+                * Matrix3::new_translation(&Vector2::new(-x, -y)),
+        ),
+        [a] => Some(Matrix3::new_rotation(a.to_radians())),
+        _ => None,
+    }
+}
+
+fn skew_x(arguments: Vec<f64>) -> Option<Matrix3<f64>> {
+    if let [a] = arguments[..] {
+        Some(Matrix2::from_vec(vec![1., 0., a.to_radians().tan(), 1.]).to_homogeneous())
+    } else {
+        None
+    }
+}
+
+fn skew_y(arguments: Vec<f64>) -> Option<Matrix3<f64>> {
+    if let [a] = arguments[..] {
+        Some(Matrix2::from_vec(vec![1., a.to_radians().tan(), 0., 1.]).to_homogeneous())
+    } else {
+        None
+    }
+}
+
 fn string_to_matrix(matrix_string: &str) -> Option<Matrix3<f64>> {
-    println!("{:?}", matrix_string);
     let match_transform = regex!(r"(\w+)\((.+)");
 
     let captures = match_transform.captures(matrix_string)?;
     let transform_type = captures.get(1)?.as_str();
     let transform_arguments = captures.get(2)?.as_str();
 
+    let transform_arguments: Vec<f64> = transform_arguments
+        .split_whitespace()
+        .filter_map(|arg| arg.parse::<f64>().ok())
+        .collect();
+
     match transform_type {
-        "matrix" => {
-            let mat = create_matrix(transform_arguments);
-            println!("{:?}", mat);
-            mat
-        }
+        "matrix" => matrix(transform_arguments),
+        "translate" => translate(transform_arguments),
+        "scale" => scale(transform_arguments),
+        "rotate" => rotate(transform_arguments),
+        "skewX" => skew_x(transform_arguments),
+        "skewY" => skew_y(transform_arguments),
         _ => None,
     }
 }
@@ -49,7 +93,6 @@ fn matrix_to_string(matrix: &Matrix3<f64>) -> String {
 
 fn merge_transform_attribute(transform_str: &str) -> String {
     let transform_string = transform_str.split_whitespace().join(" ");
-    println!("{:?}", transform_string);
     let mut result: Matrix3<f64> = Matrix3::identity();
 
     for transform in transform_string
